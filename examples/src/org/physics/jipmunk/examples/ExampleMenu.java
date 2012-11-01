c39213f2f7226a81489ed79e64ef6ee548ed95e0
jobernolte <juergen.obernolte@arcor.de>
2012-11-01 09:10:12 +0100

    Fixed mouse issue.

--- examples/src/org/physics/jipmunk/examples/ExampleMenu.java
+++ examples/src/org/physics/jipmunk/examples/ExampleMenu.java
@@ -1,60 +1,62 @@
 package org.physics.jipmunk.examples;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
 
-/** @author chris_c based on work by jobernolte & Lembcke */
+/**
+ * @author chris_c based on work by jobernolte & Lembcke
+ */
 public class ExampleMenu extends JFrame implements ActionListener {
 
-	private JComboBox combo;
-	private JButton goButton;
-	private JPanel jp;
+    private JComboBox<String> combo;
+    private JButton goButton;
+    private JPanel jp;
 
-	// list of class names to run...
-	static String examples[] = {
-			"BouncyHexagons",
-			"Convex",
-			"LogoSmash",
-			"Plink",
-			"PyramidStack",
-			"Convex"
-	};
+    // list of class names to run...
+    static String examples[] = {
+            "BouncyHexagons",
+            "Convex",
+            "LogoSmash",
+            "Plink",
+            "PyramidStack",
+            "Convex"
+    };
 
-	public ExampleMenu() {
-		setTitle("Examples Menu");
-		setSize(300, 200);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public ExampleMenu() {
+        setTitle("Examples Menu");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		combo = new JComboBox(examples);
-		goButton = new JButton("Go !");
-		goButton.addActionListener(this);
-		jp = new JPanel();
-		jp.add(combo);
-		jp.add(goButton);
-		add(jp);
-		pack();
-	}
+        combo = new JComboBox<String>(examples);
+        goButton = new JButton("Go !");
+        goButton.addActionListener(this);
+        jp = new JPanel();
+        jp.add(combo);
+        jp.add(goButton);
+        add(jp);
+        pack();
+    }
 
-	public void actionPerformed(ActionEvent e) {
-		String className = "org.physics.jipmunk.examples." + combo.getSelectedItem();
-		try {
-			ExampleBase example = (ExampleBase) Class.forName(className).newInstance();
-			example.start(640, 480);
-			hide();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+    public void actionPerformed(ActionEvent e) {
+        String className = "org.physics.jipmunk.examples." + combo.getSelectedItem();
+        try {
+            ExampleBase example = (ExampleBase) Class.forName(className).newInstance();
+            example.start(640, 480);
+            hide();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				ExampleMenu ex = new ExampleMenu();
-				ex.setVisible(true);
-			}
-		});
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ExampleMenu ex = new ExampleMenu();
+                ex.setVisible(true);
+            }
+        });
+    }
 }
